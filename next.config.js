cc04aa49f61efea33c75da62a1a86c1daa2e6c8e
Griko Nibras <git@griko.id>
2020-08-09 02:32:39 +0700

    Add custom next config

---
+++ next.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/basecamp",
+        destination: "https://3.basecamp.com/4566424",
+        permanent: true,
+      },
+      {
+        source: "/discord",
+        destination: "https://discord.gg/kx3qp5S",
+        permanent: true,
+      },
+    ];
+  },
+};
