47d8584923f4d7c9bafbbc36926809142301d283
Griko Nibras <git@griko.id>
2020-08-12 06:07:56 +0700

    Use rebrandly api for redirects

--- next.config.js
+++ next.config.js
@@ -1,21 +1,10 @@
+const redirects = require("./src/utils/fetch-redirects");
+
 module.exports = {
   experimental: {
     optimizeFonts: true,
     optimizeImages: true,
   },
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/basecamp",
-        destination: "https://3.basecamp.com/4566424",
-        permanent: true,
-      },
-      {
-        source: "/discord",
-        destination: "https://discord.gg/kx3qp5S",
-        permanent: true,
-      },
-    ];
-  },
+  redirects,
 };
