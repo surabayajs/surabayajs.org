9f39fd1b03d40ae5fc8a8126d1ae7afb2cdc7554
Griko Nibras <github+alternate@griko.id>
2020-11-20 09:29:57 +0700

    fix: use redirects and remove coc page

--- next.config.js
+++ next.config.js
@@ -1,9 +1,26 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
+const { coc } = require("./site-config");
+
 module.exports = {
   i18n: {
     defaultLocale: "id",
     locales: ["en", "id"],
   },
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/coc",
+        destination: coc.id,
+        permanent: true,
+      },
+      {
+        source: "/en/coc",
+        destination: coc.en,
+        locale: false,
+        permanent: true,
+      },
+    ];
+  },
 };
