fabfa46f2d836838d67572bb238593992e554a49
Griko Nibras <git@griko.id>
2020-11-25 13:14:47 +0700

    feat: add empty page

---
+++ src/pages/empty.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+import type { NextPage } from "next";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface EmptyPageProps {
+  //
+}
+
+const EmptyPage: NextPage<EmptyPageProps> = () => {
+  return (
+    <>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+    </>
+  );
+};
+
+export default EmptyPage;
