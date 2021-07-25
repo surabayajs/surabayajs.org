2a3d539e93692289782dc9e21e8a44ecbbef6bd1
Griko Nibras <grikomsn@icloud.com>
2021-07-25 14:24:45 +0700

    chore: remove unused
    Co-Authored-By: Joshua Nathaniel M <clsjoshua@gmail.com>

--- next-sitemap.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
-const siteConfig = require("./site-config");
-
-module.exports = {
-  siteUrl: siteConfig.url,
-  generateRobotsTxt: true,
-};
