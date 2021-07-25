364fb0cc37749be2a0f46f0af04140d79bc43286
Griko Nibras <grikomsn@icloud.com>
2021-07-25 13:05:50 +0700

    feat(next): enable experimental configs
    Co-Authored-By: Joshua Nathaniel M <clsjoshua@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 module.exports = {
+  // https://github.com/vercel/next.js/blob/3b388c346c6990c98e83357ad68263edc7081210/packages/next/server/config-shared.ts#L73-L97
+  experimental: {
+    conformance: true,
+    optimizeCss: true,
+    optimizeImages: true,
+    workerThreads: true,
+  },
+
   i18n: {
     defaultLocale: "id",
     locales: ["en", "id"],
