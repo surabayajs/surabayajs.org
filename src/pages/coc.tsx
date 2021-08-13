998d2417922a47e7d23dcdd931f68feb7f9d717a
Joshua Nathaniel M <clsjoshua@gmail.com>
2021-08-13 22:45:34 +0700

    feat: remove unused variable and add still on progress

--- src/pages/coc.tsx
+++ src/pages/coc.tsx
@@ -1,14 +1,10 @@
-/* eslint-disable @babel/new-cap */
-
 import * as React from "react";
 
 import i18n from "@/i18n";
 import { Container, Heading, VStack } from "@chakra-ui/react";
 
-import ChakraUIRenderer from "chakra-ui-markdown-renderer";
 import { GetStaticPropsContext, InferGetStaticPropsType, NextPage } from "next";
 import { NextSeo } from "next-seo";
-import ReactMarkdown from "react-markdown";
 
 export async function getStaticProps(args: GetStaticPropsContext) {
   const locale = args.locale as string;
@@ -32,12 +28,9 @@ const CodeOfConductPage: NextPage<
         </VStack>
       </Container>
       <Container as="section" maxW="6xl" pt={[4, 8]} px={[4, 8]}>
-        <ReactMarkdown
-          // @ts-expect-error ignoring unused/deprecated api
-          escapeHtml={false}
-          renderers={ChakraUIRenderer()}
-          source={i18n["coc-page"][locale] as string}
-        />
+        <Heading as="h1" color="gray.400" size="lg" textAlign="center">
+          Still on progress ...
+        </Heading>
       </Container>
     </>
   );
