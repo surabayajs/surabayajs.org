ed3577072de17eb7aa624b23aa539f9b6be9c925
burhanahmeed <burhanahmeed@gmail.com>
2021-08-15 21:58:29 +0700

    feat(lean): add learn page

--- src/pages/learn/index.tsx
+++ src/pages/learn/index.tsx
@@ -1,14 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import * as React from "react";
 
-import { Container, VStack, Text, Heading } from "@chakra-ui/react";
 import { LearningCard } from "@/components/learning-card";
-
-import { NextPage, GetStaticProps } from "next";
-import { NextSeo } from "next-seo";
 import i18n from "@/i18n";
+import {
+  Button,
+  Container,
+  Heading,
+  Stack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+
+import { GetStaticProps, NextPage } from "next";
+import { NextSeo } from "next-seo";
 
 interface LearnPageProps {
   locale: string;
+}
+
+interface StateProperties {
+  title: string;
+  description: string;
+  url: string;
 }
 
 export const getStaticProps: GetStaticProps<LearnPageProps> = async (args) => {
@@ -21,6 +35,35 @@ export const getStaticProps: GetStaticProps<LearnPageProps> = async (args) => {
 };
 
 const LearnPage: NextPage<LearnPageProps> = ({ locale }) => {
+  const [category, setCategory] = React.useState("repository");
+  const [resources, setResources] = React.useState<StateProperties[]>([]);
+
+  const categories = [
+    {
+      id: "repository",
+      text: "Repository",
+      url: "https://raw.githubusercontent.com/surabayajs/learning-resources/master/repositories.json",
+    },
+    {
+      id: "video-course",
+      text: "Video course",
+      url: "https://raw.githubusercontent.com/surabayajs/learning-resources/master/video-courses.json",
+    },
+  ];
+
+  const fetchData = (url): void => {
+    fetch(url)
+      .then((records) => records.json())
+      .then((results) => setResources(results));
+  };
+
+  React.useEffect(() => {
+    const categoryData = categories.find((cat) => cat.id === category);
+    if (categoryData) {
+      fetchData(categoryData.url);
+    }
+  }, [category]);
+
   return (
     <>
       <NextSeo title={i18n["learn-title"][locale]} />
@@ -30,10 +73,28 @@ const LearnPage: NextPage<LearnPageProps> = ({ locale }) => {
           <Text pb={4}>{i18n["learn-subtitle"][locale]}</Text>
         </VStack>
       </Container>
+      <Container as="section" maxW="6xl" pb="4" px={[4, 8]}>
+        <Stack
+          align="center"
+          borderBottom="1px gray solid"
+          direction="row"
+          py={[8, 4]}
+        >
+          {categories.map((cat, idx) => (
+            <Button
+              key={idx}
+              onClick={() => setCategory(cat.id)}
+              variant={cat.id === category ? "solid" : "ghost"}
+            >
+              {cat.text}
+            </Button>
+          ))}
+        </Stack>
+      </Container>
       <Container as="section" maxW="6xl" px={[4, 8]}>
         <VStack spacing={[2, 4]}>
-          {[1, 2, 3, 4].map((el) => {
-            return <LearningCard key={el} />;
+          {resources.map((el, idx) => {
+            return <LearningCard key={idx} data={el} />;
           })}
         </VStack>
       </Container>
