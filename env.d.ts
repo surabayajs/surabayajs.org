44d98d574a727cf34c7e9d6ed06f3881ecf9beb4
Griko Nibras <git@griko.id>
2022-03-10 06:54:03 +0700

    chore(types): update declarations

--- env.d.ts
+++ env.d.ts
@@ -1,13 +1,5 @@
-declare module "*.css" {
-  const classes: { readonly [key: string]: string };
-  export default classes;
-}
-
 declare namespace NodeJS {
   interface ProcessEnv {
-    readonly VERCEL?: "1";
-    readonly VERCEL_ENV: "development" | "preview" | "production";
-
     readonly CONTENTFUL_API_KEY: string;
     readonly CONTENTFUL_SPACE_ID: string;
   }
