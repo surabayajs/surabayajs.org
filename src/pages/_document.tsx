1681ad43aeb7f44fdccc52d236826662258be8b2
Griko Nibras <git@griko.id>
2022-03-10 07:05:38 +0700

    chore(pages)): remove unused getInitialProps

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,11 +6,6 @@ import { ColorModeScript } from "@chakra-ui/react";
 import NextDocument, { Head, Html, Main, NextScript } from "next/document";
 
 export default class Document extends NextDocument {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await NextDocument.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
   render() {
     return (
       <Html>
