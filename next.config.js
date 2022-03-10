239ac6acfe6711a1b7e2d7a7bbf1bcb6e2ca3210
Griko Nibras <git@griko.id>
2022-03-10 07:01:23 +0700

    chore(config): simplify next config
    Signed-off-by: Griko Nibras <git@griko.id>

--- next.config.js
+++ next.config.js
@@ -7,45 +7,26 @@ module.exports = {
       {
         source: "/(.*)",
         headers: [
-          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control
-          {
-            key: "Cache-Control",
-            value: "public, s-maxage=1, stale-while-revalidate=59",
-          },
-
-          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
-          // Opt-out of Google FLoC: https://amifloced.org/
           {
             key: "Permissions-Policy",
-            value:
-              "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+            value: `camera=(), microphone=(), geolocation=(), interest-cohort=()`,
           },
-
-          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
           {
             key: "Referrer-Policy",
             value: "origin-when-cross-origin",
           },
-
-          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
           {
             key: "Strict-Transport-Security",
             value: "max-age=31536000; includeSubDomains; preload",
           },
-
-          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
           {
             key: "X-Frame-Options",
             value: "DENY",
           },
-
-          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
           {
             key: "X-Content-Type-Options",
             value: "nosniff",
           },
-
-          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
           {
             key: "X-DNS-Prefetch-Control",
             value: "on",
@@ -82,10 +63,6 @@ module.exports = {
   // },
 
   // https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
-  /**
-   * @param {import("webpack").Configuration} config
-   * @param {{dev:boolean;isServer:boolean}} opts
-   */
   webpack(config, { dev, webpack }) {
     config.plugins.push(
       new webpack.DefinePlugin({
